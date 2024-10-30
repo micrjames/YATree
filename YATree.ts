625842c9dc3a0069399cdecbe124bb7d055b9527
Michael James <micrjames921@gmail.com>
2024-10-30 15:08:18 -0700

    completed tests on section 'Created: With no Nodes'

--- YATree.ts
+++ YATree.ts
@@ -1,30 +1,28 @@
-import { Node } from "./Node";
+import { Node } from "./NodeCollection/Node";
 
 export class YATree<T> {
-   private root: Node<T> | null;
+   private _root: Node<T> | null;
    private _height: number;
    private _level: number;
    // leaf: node with no children.
 
    constructor() {
-	  this.root = null;
+	  this._root = null;
 	  this._height = 0;
 	  this._level = 0;
    }
 
    // height: maximum depth of subtree node and farthest leaf
    get height(): number {
-	if(!this.root) return 0
+	  if(!this._root) return 0
    }
 
    // level: 1 + num edges between root and node.
    get level(): number {
-	  if(!this.root) return 0
+	  if(!this._root) return 0
+   }
+
+   get root(): Node<T> {
+	  return this._root;
    }
 }
-
-
-
-
-
-
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,5 @@
 import { YATree } from "../YATree";
+import { Node } from "../NodeCollection/Node";
 
 describe("A YATree", () => {
    let tree: YATree<number>;
@@ -6,21 +7,31 @@ describe("A YATree", () => {
 	  tree = new YATree();
    });
    describe("Created", () => {
-	  test.todo("Should exist.");
-	  test.todo("Should have a single node.");
-	  test.todo("Should only exist as the 'root node'.");
-	  test.todo("Should have a 'level' of 1.");
-	  test.todo("Should have a 'height' of 1.");
+	  let treeRoot: Node<number>;
+	  let height: number;
+	  let level: number;
+	  beforeAll(() => {
+		 treeRoot = tree.root;
+		 height = tree.height;
+		 level = tree.level;
+	  });
+	  describe("With no nodes", () => {
+		 test("Should exist.", () => {
+			expect(tree).toBeDefined();
+		 });
+		 test("Should have no nodes.", () => {
+			expect(treeRoot).toBeNull();
+		 });
+		 test("Should have no Key value.", () => {
+			const nodeKey = treeRoot?.Key;
+			expect(nodeKey).toBeUndefined();
+		 });
+		 test("Should have a 'level' of 0.", () => {
+			expect(level).toBe(0);
+		 });
+		 test("Should have a 'height' of 0.", () => {
+			expect(height).toBe(0);
+		 });
+	  });
    });
 });
-/*
-	let rootNode = new Node<string>(‘Root’);
-	let child1 = new Node<string>(‘Child 1’);
-	let child2 = new Node<string>(‘Child 2’);
-	let subChild = new Node<string>(‘Sub Child’);
-
-	// Add child nodes
-	rootNode.addChild(child1);
-	rootNode.addChild(child2);
-	child1.addChild(subChild);
-*/
