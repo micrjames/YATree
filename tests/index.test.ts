cc57a7cbca4b8d6bbbeb12ab855646f1a991f12b
Michael James <micrjames921@gmail.com>
2024-11-13 19:29:58 -0700

    refactored test suite to include tests for 'traversal'

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -90,50 +90,70 @@ describe("A YATree", () => {
 			   treeRootChildrenKey3 = 3;
 			   treeRootChildrenKeys = [treeRootChildrenKey2, treeRootChildrenKey3];
 			   treeRootChildrenKeysSize = treeRootChildrenKeys.length;
-			   treeRootChildCollection = new NodeCollection(new Node(treeRootChildrenKeys[0]),
-															new Node(treeRootChildrenKeys[1]));
-			   tree = new YATree(new Node(nodeKeyValue,
-										  treeRootChildCollection.item(0),
-										  treeRootChildCollection.item(1)
-								));
-			   treeRoot = tree.root;
-			   treeRootChildren = treeRoot.children;
-			   treeRootChildrenSize = treeRootChildren.size;
-			   treeRootFirstChild = treeRoot.children.next().value;
-			   treeRootLastChild = treeRoot.children.next().value;
-			   level = tree.level;
-			   height = tree.height;
-			   nLevel++;nHeight++;
 			});
-			test("Should have children.", () => {
-			   expect(treeRootChildren).toBeDefined();
+			describe("By Object Inspection", () => {
+			   beforeAll(() => {
+				  treeRootChildCollection = new NodeCollection(new Node(treeRootChildrenKeys[0]),
+															   new Node(treeRootChildrenKeys[1]));
+				  tree = new YATree(new Node(nodeKeyValue,
+											 treeRootChildCollection.item(0),
+											 treeRootChildCollection.item(1)
+								   ));
+				  treeRoot = tree.root;
+				  treeRootChildren = treeRoot.children;
+				  treeRootChildrenSize = treeRootChildren.size;
+				  treeRootFirstChild = treeRoot.children.next().value;
+				  treeRootLastChild = treeRoot.children.next().value;
+				  level = tree.level;
+				  height = tree.height;
+				  nLevel++;nHeight++;
+			   });
+			   test("Should have children.", () => {
+				  expect(treeRootChildren).toBeDefined();
+			   });
+			   test(`Should have ${treeRootChildrenKeysSize} children.`, () => {
+				  expect(treeRootChildrenSize).toBe(treeRootChildrenKeysSize);
+			   });
+			   test(`Should have ${treeRootChildrenKey2} as the key of the first child node.`, () => {
+				  const treeRootFirstChildKey = treeRootFirstChild.Key;
+				  expect(treeRootFirstChildKey).toBe(treeRootChildrenKey2);
+			   });
+			   test(`Should have ${treeRootChildrenKey3} as the key of the second/last child node.`, () => {
+				  const treeRootLastChildKey = treeRootLastChild.Key;
+				  expect(treeRootLastChildKey).toBe(treeRootChildrenKey3);
+			   });
+			   test("Should have the children at the level of one more than at the root node.", () => {
+				  const childLevel = level + 1;
+				  expect(childLevel).toBe(nLevel);
+			   });
+			   test("Should have ${treeRootFirstChild} as the first child sibling at that level.", () => {
+				  const firstChild = treeRootChildCollection.next().value;
+				  expect(treeRootFirstChild).toEqual(firstChild);
+			   });
+			   test("Should have ${treeRootLastChild} as the second/last child sibling at that level.", () => {
+				  const lastChild = treeRootChildCollection.next().value;
+				  expect(treeRootLastChild).toEqual(lastChild);
+			   });
+			   test("Should have height of one more than at the root node", () => {
+				  const childHeight = height;
+				  expect(childHeight).toBe(nHeight);
+			   });
 			});
-			test(`Should have ${treeRootChildrenKeysSize} children.`, () => {
-			   expect(treeRootChildrenSize).toBe(treeRootChildrenKeysSize);
-			});
-			test(`Should have ${treeRootChildrenKey2} as the key of the first child node.`, () => {
-			   const treeRootFirstChildKey = treeRootFirstChild.Key;
-			   expect(treeRootFirstChildKey).toBe(treeRootChildrenKey2);
-			});
-			test(`Should have ${treeRootChildrenKey3} as the key of the second/last child node.`, () => {
-			   const treeRootLastChildKey = treeRootLastChild.Key;
-			   expect(treeRootLastChildKey).toBe(treeRootChildrenKey3);
-			});
-			test("Should have the children at the level of one more than at the root node.", () => {
-			   const childLevel = level + 1;
-			   expect(childLevel).toBe(nLevel);
-			});
-			test("Should have ${treeRootFirstChild} as the first child sibling at that level.", () => {
-			   const firstChild = treeRootChildCollection.next().value;
-			   expect(treeRootFirstChild).toEqual(firstChild);
-			});
-			test("Should have ${treeRootLastChild} as the second/last child sibling at that level.", () => {
-			   const lastChild = treeRootChildCollection.next().value;
-			   expect(treeRootLastChild).toEqual(lastChild);
-			});
-			test("Should have height of one more than at the root node", () => {
-			   const childHeight = height;
-			   expect(childHeight).toBe(nHeight);
+			describe("By Node Traversal", () => {
+			   beforeAll(() => {
+				  treeRootChildCollection = new NodeCollection(new Node(treeRootChildrenKeys[0]),
+															   new Node(treeRootChildrenKeys[1]));
+				  tree = new YATree(new Node(nodeKeyValue,
+											 treeRootChildCollection.item(0),
+											 treeRootChildCollection.item(1)
+								   ));
+				  treeRootFirstChild = tree.traverseTo(new Node(treeRootChildrenKey3));
+				  level = tree.level;
+				  height = tree.height;
+				  console.log(treeRootFirstChild, level, height);
+			   });
+			   test.todo("Should have a first child.");
+			   test.todo("Should have a last child.");
 			});
 		 });
 	  });
